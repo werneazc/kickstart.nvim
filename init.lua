989145a75eec695e4590c9c5359f74fc1d814fc8
Andre Werner <werneazc@gmail.com>
2024-11-05 10:09:53 +0100

    init.lua: Enable availability for nerd-fonts
    Signed-off-by: Andre Werner <werneazc@gmail.com>

--- init.lua
+++ init.lua
@@ -92,7 +92,7 @@ vim.g.maplocalleader = ' '
 vim.g.editorconfig = true
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
